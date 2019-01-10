93c17eb617fb4acb01862a9174df3b15201ac017
faruk hadzigrahic <faruk.hadzigrahic@elev.ga.lbs.se>
2019-01-10 11:02:20 +0100

    menustuff

---
+++ Assets/Bottletoggle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bottletoggle : MonoBehaviour
+{
+    public void Togglechanged(bool state)
+    {
+        GetComponent<Button>().interactable = state;
+    }
+}
